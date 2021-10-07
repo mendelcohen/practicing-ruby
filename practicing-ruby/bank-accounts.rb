7367c7f1c29c556a9af5d6100ac545fc6ba892c5
Mendel Cohen <mmhakohen@gmail.com>
2021-10-07 11:15:21 -0400

    prints out each account info seperately

--- practicing-ruby/bank-accounts.rb
+++ practicing-ruby/bank-accounts.rb
@@ -3,14 +3,20 @@ bank_account = {}
 5.times do
   p "Enter your first name."
   first_name = gets.chomp
-  bank_account["first name"] = first_name
+  bank_account["FIRST NAME"] = first_name
   p "Enter your last name."
   last_name = gets.chomp
-  bank_account["last name"] = last_name
+  bank_account["LAST NAME"] = last_name
   p "Enter your email address."
   email_address = gets.chomp
-  bank_account["email address"] = email_address
-  bank_account["Account Number"] = rand 10000000000
+  bank_account["EMAIL"] = email_address
+  bank_account["ACCT #"] = rand 10000000000
   bank_accounts << bank_account
-  p bank_accounts
+  puts bank_accounts
+end
+bank_accounts.each do |account|
+  account.each do |key, prop|
+    puts "#{key}: #{prop}"
+  end
+  puts "\r\n"
 end
