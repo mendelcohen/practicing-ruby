759d0b1c951513ce8c99eb7ae8b2df20863395a8
Mendel Cohen <mmhakohen@gmail.com>
2021-11-12 12:40:28 -0500

    retrieves account info with number

--- practicing-ruby/bank-accounts.rb
+++ practicing-ruby/bank-accounts.rb
@@ -15,9 +15,32 @@ bank_accounts = []
   bank_accounts << bank_account
   puts bank_accounts
 end
+
 bank_accounts.each do |account|
   account.each do |key, prop|
     puts "#{key}: #{prop}"
   end
   puts "\r\n"
 end
+p bank_accounts[0]["ACCT #"]
+puts "Enter your account #"
+number = gets.chomp
+i = 0
+while i < bank_accounts.length
+  if number.to_i === bank_accounts[i]["ACCT #"]
+    puts bank_accounts[i]
+    break
+  elsif i < bank_accounts.length - 1
+    i += 1
+  else
+    puts "Account Not Found"
+    break
+  end
+end
+# bank_accounts.each do |account|
+#   if number.to_i === account["ACCT #"]
+#     puts account
+#     break
+#   end
+#   puts "Account Not Found"
+# end
